96e1dca0f9cae652989d86c30309b4b4525e0a68
RottenKiwi <markkittiphat@gmail.com>
2023-06-13 21:48:17 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "shopUtils/snack.h"
+
+int main()
+{
+    cout << snackList[0] << endl;
+}
---
+++ shopUtils/snack.h
@@ -0,0 +1,35 @@
+#ifndef snack_h
+#define snack_h
+
+std::string snackList[] = {
+    "Salted peanuts",
+    "Pretzels",
+    "Potato chips",
+    "Banana chips",
+    "Apple chips",
+    "Pork rinds",
+    "Cheese dips",
+    "Mixed nuts",
+    "Popcorn",
+    "Beef jerky",
+    "Crackers",
+    "Corn snacks",
+    "Cheese",
+    "Chocolates",
+    "Candies",
+    "Ice cream",
+    "Milkshakes",
+    "Pastries",
+    "Pies",
+    "Cookies",
+    "Cakes",
+    "Puddings",
+    "Cereal",
+    "Brownies",
+    "Fudge",
+    "Peanut butter",
+    "Pop tarts",
+    "Pancakes",
+    "Scrones"};
+
+#endif
