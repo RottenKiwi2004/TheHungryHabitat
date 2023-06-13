bf349c5462f975f6f88b97dfa03b3222ed3eaa79
RottenKiwi <markkittiphat@gmail.com>
2023-06-13 22:35:09 +0700

    Added animal list

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 #include "shopUtils/snack.h"
+#include "shopUtils/animal.h"
 
 int main()
 {
     cout << snackList[0] << endl;
+    cout << animalList[0] << endl;
 }
---
+++ shopUtils/animal.h
@@ -0,0 +1,22 @@
+#ifndef animal_h
+#define animal_h
+
+std::string animalList[] = {
+    "Fallow Deer",
+    "Tasmanian Devil",
+    "Koala",
+    "Dingo",
+    "Red Deer",
+    "Emu",
+    "European Rabbit",
+    "Green Sea Turtle",
+    "Mulgara",
+    "Platypus",
+    "Wandering Albatross",
+    "Short-Beake Echidna",
+    "Saltwater Crocodile",
+    "Spinner Dolphin",
+    "Black Flying Fox",
+    "Kangaroo"};
+
+#endif
