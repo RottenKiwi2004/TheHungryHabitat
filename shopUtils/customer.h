bb1e794106a2b20190989aa3905a5cd36499330d
Pakin-pan <pakin.pan@student.mahidol.edu>
2023-06-15 05:44:37 +0700

    Can't sleep at 5 am

--- shopUtils/customer.h
+++ shopUtils/customer.h
@@ -3,7 +3,17 @@
 
 class Customer
 {
+    private:
+        std::string name;
+        double money;        
+    public:
+        Customer(std::string,double);
     // Implement customer, they can order food, pay for it, they have some amount of money etc.
 };
+
+Customer(string n,double money){
+    
+};
+
 
 #endif
